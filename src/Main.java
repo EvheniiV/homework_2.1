a2efb3dbf5c0a8e0b537669fbfabd011b5554806
Jenyokgm <jenyokgm@gmail.com>
2023-07-02 21:29:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15651;
+        int bonusPrice = 20;
+        int bonus;
+        bonus = ticketPrice / bonusPrice;
+        System.out.println(bonus);
+    }
+}
